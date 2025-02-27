cc16a976ccbebb98d175ca65712299daac3d5297
Cavaliere <s.lorenzo@studenti.unina.it>
2025-02-27 17:58:58 +0100

    modifica codice link api in AUTH

--- src/main/java/it/unina/dietiestates25/dto/request/SignUpRequest.java
+++ src/main/java/it/unina/dietiestates25/dto/request/SignUpRequest.java
@@ -11,9 +11,11 @@ public record SignUpRequest(
         @NotBlank(message = "Username cannot be null or blank")
         @Size(max = 20, min = 3, message = "Username must be between 3 and 20 characters")
         String username,
+        
         @NotBlank(message = "Email cannot be null or blank")
         @Email(message = "Email not well-formed")
         String email,
+        
         @NotBlank(message = "Password cannot be null or blank")
         @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters")
         @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$£%^&+=!]).*$",
