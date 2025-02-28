355f8049749789963bbcd2ae4a89cca9aa5bbf08
Sperob98 <rbrna98@gmail.com>
2025-02-28 18:35:07 +0100

    creato la classe per implementare getUserCurrent dal contex

---
+++ src/main/java/it/unina/dietiestates25/utils/UserContex.java
@@ -0,0 +1,14 @@
+package it.unina.dietiestates25.utils;
+
+import it.unina.dietiestates25.entity.User;
+
+public class UserContex {
+
+    public static User getUserCurrent(){
+
+        //TODO capire come ottente user corrente dal contex
+        User userCurrent = null;
+
+        return userCurrent;
+    }
+}
