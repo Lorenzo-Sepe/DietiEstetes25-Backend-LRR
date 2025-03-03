ebe016fbcfaa83dd5e64c89308ea6c081a10bcfe
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-03-03 10:28:37 +0100

    traduzione Documentazione Login

--- src/main/java/it/unina/dietiestates25/dto/request/SignUpRequest.java
+++ src/main/java/it/unina/dietiestates25/dto/request/SignUpRequest.java
@@ -1,6 +1,5 @@
 package it.unina.dietiestates25.dto.request;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -8,18 +7,18 @@ import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
 
-        @NotBlank(message = "Username cannot be null or blank")
-        @Size(max = 20, min = 3, message = "Username must be between 3 and 20 characters")
+        @NotBlank(message = "Il nome utente non può essere nullo o vuoto")
+        @Size(max = 20, min = 3, message = "Il nome utente deve essere compreso tra 3 e 20 caratteri")
         String username,
-        
-        @NotBlank(message = "Email cannot be null or blank")
-        @Email(message = "Email not well-formed")
+
+        @NotBlank(message = "L'email non può essere nulla o vuota")
+        @Email(message = "Formato email non valido")
         String email,
-        
-        @NotBlank(message = "Password cannot be null or blank")
-        @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters")
+
+        @NotBlank(message = "La password non può essere nulla o vuota")
+        @Size(min = 8, max = 16, message = "La password deve essere compresa tra 8 e 16 caratteri")
         @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$£%^&+=!]).*$",
-                message = "Password must contain at least one digit, one lowercase, one uppercase letter and one special character")
+                message = "La password deve contenere almeno un numero, una lettera minuscola, una lettera maiuscola e un carattere speciale")
         String password
 ) {
 }
