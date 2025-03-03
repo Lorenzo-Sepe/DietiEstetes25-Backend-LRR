23f8f728bfe0f0d323a88a4ac3fc6cb72f6fd036
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-03-03 09:57:03 +0100

    Tradotto la documentazione in Italiano

--- src/main/java/it/unina/dietiestates25/controller/AgenziaImmobiliareController.java
+++ src/main/java/it/unina/dietiestates25/controller/AgenziaImmobiliareController.java
@@ -26,40 +26,35 @@ public class AgenziaImmobiliareController {
     private final AgenziaImmobiliareService agenziaImmobiliareService;
     private final AuthService authService;
 
-    //regsitra la tua agenzia immobiliare
+    // Registra una nuova agenzia immobiliare
     @PostMapping("/pb/agenzia/")
     @Operation(
-            summary = "ADD A NEW AGENCY",
-            description = "Method to add a new agency to the database",
+            summary = "AGGIUNGI UNA NUOVA AGENZIA",
+            description = "Metodo per aggiungere una nuova agenzia immobiliare nel database",
             tags = {"Agenzia"})
     public ResponseEntity<String> createAgenzia(@RequestBody AgenziaImmobiliareRequest request) {
-
-        return new ResponseEntity<String>(agenziaImmobiliareService.createAgenzia(request),HttpStatus.CREATED);
+        return new ResponseEntity<>(agenziaImmobiliareService.createAgenzia(request), HttpStatus.CREATED);
     }
 
-    //da cambiare in privato quando il server è funzionante
+    // Aggiungi un nuovo agente immobiliare (da rendere privato quando il server è funzionante)
     @PostMapping("/v1/addAgent/")
     @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(
-            summary = "ADD A NEW AGENT",
-            description = "Method to add a new agent to the database",
+            summary = "AGGIUNGI UN NUOVO AGENTE",
+            description = "Metodo per aggiungere un nuovo agente immobiliare nel database",
             tags = {"Agenzia"})
     public ResponseEntity<User> createAgente(@RequestBody NewAgentRequest request,
                                              @AuthenticationPrincipal UserDetails userDetails) {
-
-        return new ResponseEntity<User>(authService.createAgente(request, userDetails), HttpStatus.CREATED);
+        return new ResponseEntity<>(authService.createAgente(request, userDetails), HttpStatus.CREATED);
     }
 
-    //restituisce tutte le agenzie
+    // Restituisce tutte le agenzie immobiliari presenti nel database
     @GetMapping("/pb/agenzia/")
     @Operation(
-            summary = "GET ALL AGENCIES",
-            description = "Method to get all agencies from the database",
+            summary = "OTTIENI TUTTE LE AGENZIE",
+            description = "Metodo per ottenere l'elenco di tutte le agenzie immobiliari presenti nel database",
             tags = {"Agenzia"})
     public ResponseEntity<List<AgenziaImmobiliareResponse>> getAgenzie(@RequestParam Pageable pageable) {
         return ResponseEntity.ok(agenziaImmobiliareService.getAgenzie());
     }
-
-
-
 }
