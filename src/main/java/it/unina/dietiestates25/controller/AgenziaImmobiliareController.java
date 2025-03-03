6ff9dfbf5f9a3f0e76ea473aab24a4ed8a01b2cd
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-03-03 17:34:43 +0100

    creato esempio di salvataggio immagini

--- src/main/java/it/unina/dietiestates25/controller/AgenziaImmobiliareController.java
+++ src/main/java/it/unina/dietiestates25/controller/AgenziaImmobiliareController.java
@@ -30,31 +30,30 @@ public class AgenziaImmobiliareController {
             summary = "INSERISCI LA TUA AGENZIA IMMOBILIARE",
             description = "Metodo per aggiungere una nuova agenzia immobiliare nel database",
             tags = {"Agenzia"})
-    public ResponseEntity<String> createAgenzia(@RequestBody AgenziaImmobiliareRequest request) {
+    public ResponseEntity<String> createAgenzia(@ModelAttribute AgenziaImmobiliareRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(agenziaImmobiliareService.createAgenzia(request));
     }
 
-    // Aggiungi un nuovo agente immobiliare (da rendere privato quando il server è funzionante)
-    @PostMapping("/v1/addAgent/")
+    @PostMapping("/agenzia/dipendete")
     @PreAuthorize("hasAuthority('ADMIN')")
     @Operation(
             summary = "AGGIUNGI UN NUOVO AGENTE",
             description = "Metodo per aggiungere un nuovo agente immobiliare nel database",
             tags = {"Agenzia"})
-    public ResponseEntity<String> createAgente(@RequestBody DipendenteRequest request) {
+    public ResponseEntity<String> createAgente(@ModelAttribute DipendenteRequest request) {
 
         return new ResponseEntity<>(agenziaImmobiliareService.aggiungiDipendete(request), HttpStatus.CREATED);
     }
 
-    // Restituisce tutte le agenzie immobiliari presenti nel database
+
     @GetMapping("/pb/agenzia/")
     @Operation(
             summary = "OTTIENI TUTTE LE AGENZIE",
             description = "Metodo per ottenere l'elenco di tutte le agenzie immobiliari presenti nel database",
             tags = {"Agenzia"})
-    public ResponseEntity<List<AgenziaImmobiliareResponse>> getAgenzie(@RequestParam Pageable pageable) {
+    public ResponseEntity<List<AgenziaImmobiliareResponse>> getAgenzie(@ModelAttribute Pageable pageable) {
         return ResponseEntity.ok(agenziaImmobiliareService.getAgenzie());
     }
 }
