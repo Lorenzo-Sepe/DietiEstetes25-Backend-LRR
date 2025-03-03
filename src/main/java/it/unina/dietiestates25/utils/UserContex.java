8bfe833ecd0434100257fc8ce1642198794ff3d6
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-03-03 09:51:59 +0100

    Update UserContex.java

--- src/main/java/it/unina/dietiestates25/utils/UserContex.java
+++ src/main/java/it/unina/dietiestates25/utils/UserContex.java
@@ -1,19 +1,22 @@
 package it.unina.dietiestates25.utils;
 
 import it.unina.dietiestates25.entity.User;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserContex {
 
-    public static User getUserCurrent(){
+    public static User getUserCurrent() {
+        Authentication autenticazione = SecurityContextHolder.getContext().getAuthentication();
 
-        //TODO capire come ottente user corrente dal contex
-        User userCurrent = User.builder()
-                .id(2)
-                .email("raimondo@gmail.com")
-                .username("Raimon")
-                .build();
+        if (autenticazione != null && autenticazione.getPrincipal() instanceof UserDetails) {
+            UserDetails dettagliUtente = (UserDetails) autenticazione.getPrincipal();
 
-        return userCurrent;
+            if (dettagliUtente instanceof User) {
+                return (User) dettagliUtente;
+            }
+        }
+        return null; // Se non c'è alcun utente autenticato
     }
 }
