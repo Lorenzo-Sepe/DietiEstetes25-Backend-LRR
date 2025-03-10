c602ea117038c533ff7ff204e8631732685139ad
Cavaliere <s.lorenzo@studenti.unina.it>
2025-03-10 16:07:12 +0100

    sistemata api key

--- src/main/java/it/unina/dietiestates25/repository/UserRepository.java
+++ src/main/java/it/unina/dietiestates25/repository/UserRepository.java
@@ -2,9 +2,6 @@ package it.unina.dietiestates25.repository;
 
 import it.unina.dietiestates25.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -15,14 +12,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
 
-    @Modifying
-    @Query(value="UPDATE _user SET password = :password WHERE id = :id", nativeQuery = true)
-    void updatePassword(String password, int id);
-
-    boolean existsByUsernameAndIdNot(String username, int id);
-    boolean existsByEmailAndIdNot(String email, int id);
-
     int countByEmail(String email);
-
-
 }
