8a980b75d5ba2d64ed665b78a6794b6a30a2a3af
RaimondoMorosini <96730710+RaimondoMorosini@users.noreply.github.com>
2025-03-10 17:13:34 +0100

    Update FiltroAnnuncio.java

--- src/main/java/it/unina/dietiestates25/dto/request/FiltroAnnuncio.java
+++ src/main/java/it/unina/dietiestates25/dto/request/FiltroAnnuncio.java
@@ -1,21 +1,29 @@
 package it.unina.dietiestates25.dto.request;
 
+import it.unina.dietiestates25.entity.enumeration.TipoContratto;
 import it.unina.dietiestates25.entity.enumeration.TipologiaImmobile;
 import lombok.Data;
 
 @Data
 public class FiltroAnnuncio {
+    //pageable
     private Integer numeroPagina;
     private Integer numeroDiElementiPerPagina;
+
     private String titolo;
     private TipologiaImmobile tipologiaImmobile;
+    private TipoContratto tipologiaContratto;
     private Double prezzoMin;
     private Double prezzoMax;
     private Integer metriQuadriMin;
     private Integer metriQuadriMax;
+
+    //posizione
+    private String provincia;
     private Double latCentro;
     private Double lonCentro;
     private Double raggioKm;
+
     private Boolean balconi;
     private Boolean garage;
     private Boolean pannelliSolari;
