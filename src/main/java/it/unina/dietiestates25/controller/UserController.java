3bdaf2b6d93ed82399ed70a88382d22d8486a604
Sperob98 <rbrna98@gmail.com>
2025-03-14 22:45:29 +0100

    Fixato il bug per la disattivazione delle categorie

--- src/main/java/it/unina/dietiestates25/controller/UserController.java
+++ src/main/java/it/unina/dietiestates25/controller/UserController.java
@@ -3,8 +3,10 @@ package it.unina.dietiestates25.controller;
 import it.unina.dietiestates25.dto.request.CategoriaNotificaRequest;
 import it.unina.dietiestates25.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +17,8 @@ public class UserController {
 
     //Modifica Sottoscrizioni
     @PatchMapping("/utente/sottoscrizioni")
-    public void modificaSottoscrizioni(CategoriaNotificaRequest request) {
-        userService.modificaSottoscrizioni(request);
+    public ResponseEntity<String> modificaSottoscrizioni(@RequestBody CategoriaNotificaRequest request) {
+
+        return ResponseEntity.ok(userService.modificaSottoscrizioni(request));
     }
 }
