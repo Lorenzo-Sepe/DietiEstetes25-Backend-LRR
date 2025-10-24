81043f57aec1fc657a7141fe34a1f9a2ddaef5ca
Roberto Spena <rob.spena@studenti.unina.it>
2025-10-24 22:47:12 +0200

    Refactoring: eliminato un import non usato

--- src/main/java/it/unina/dietiestates25/entity/Contratto.java
+++ src/main/java/it/unina/dietiestates25/entity/Contratto.java
@@ -1,6 +1,5 @@
 package it.unina.dietiestates25.entity;
 
-import it.unina.dietiestates25.dto.request.agenziaImmobiliare.ContrattoRequest;
 import it.unina.dietiestates25.entity.common.CreationUpdate;
 import jakarta.persistence.*;
 import lombok.*;
