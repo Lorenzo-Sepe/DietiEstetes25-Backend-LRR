7fab1f829d46e800f809b0a1037a4d93c456da24
Roberto Spena <rob.spena@studenti.unina.it>
2025-10-24 22:44:46 +0200

    Refatoring: eliminato tutti gli import non utilizzati

--- src/main/java/it/unina/dietiestates25/repository/AnnuncioImmobiliareRepository.java
+++ src/main/java/it/unina/dietiestates25/repository/AnnuncioImmobiliareRepository.java
@@ -1,14 +1,10 @@
 package it.unina.dietiestates25.repository;
 
-import it.unina.dietiestates25.dto.response.AnnuncioImmobiliareResponse;
 import it.unina.dietiestates25.entity.AnnuncioImmobiliare;
 import it.unina.dietiestates25.entity.User;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
